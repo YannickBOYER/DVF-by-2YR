45db126c125f1d7bb593c07e2a8482c4cff479a8
rpeignet <robin.peignet.contact@gmail.com>
2024-01-07 20:05:28 +0100

    clean code + import.properties

--- DVF/src/main/java/fr/esgi/DVF/service/impl/LigneTransactionServiceImpl.java
+++ DVF/src/main/java/fr/esgi/DVF/service/impl/LigneTransactionServiceImpl.java
@@ -13,24 +13,21 @@ import java.io.*;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 import java.util.zip.GZIPInputStream;
 
 @Service
 public class LigneTransactionServiceImpl implements LigneTransactionService {
     private LigneTransactionRepository ligneTransactionRepository;
-
-    private final String csvCompresedFileUrlPath = "https://files.data.gouv.fr/geo-dvf/latest/csv/2023/full.csv.gz";
-    private final String localFilePath = "doc/csv/full.csv";
+    private final Properties importProperties = new Properties();
     private CSVParser csvParser;
-
-    private final int tailleInterval = 100000;
     private int nbrLigneIgnoree;
     private boolean isImportTermine = false;
-
 
     public LigneTransactionServiceImpl(LigneTransactionRepository ligneTransactionRepository){
         try{
             this.ligneTransactionRepository = ligneTransactionRepository;
+            this.importProperties.load(new FileInputStream("src/main/resources/import.properties"));
             this.csvParser = getCSVParser();
         }catch (Exception exception){
             System.out.println(exception.getMessage());
@@ -48,7 +45,7 @@ public class LigneTransactionServiceImpl implements LigneTransactionService {
             if(!isImportTermine){
                 if(csvParser.iterator().hasNext()){
                     System.out.println("Importation de 100 000 lignes en base H2...");
-                    for (int i = 0; i < tailleInterval; i++){
+                    for (int i = 0; i < Integer.parseInt(importProperties.getProperty("nbrIntervalLigneParImport")); i++){
                         if(csvParser.iterator().hasNext()){
                             CSVRecord csvRecord = csvParser.iterator().next();
                             importerLigneTransactionByCSVRecord(csvRecord);
@@ -91,18 +88,18 @@ public class LigneTransactionServiceImpl implements LigneTransactionService {
 
     private CSVParser getCSVParser() throws Exception{
         downloadAndDecompressGzip();
-        File file = new File(localFilePath);
+        File file = new File(importProperties.getProperty("localFilePath"));
         Reader reader = new InputStreamReader(new BufferedInputStream(file.toURL().openStream()));
         CSVFormat csvFormat = CSVFormat.newFormat(',').builder().setHeader().setSkipHeaderRecord(true).build();
         return new CSVParser(reader, csvFormat);
     }
 
     private void downloadAndDecompressGzip() throws IOException {
-        URL url = new URL(csvCompresedFileUrlPath);
+        URL url = new URL(importProperties.getProperty("csvCompresedFileUrlPath"));
 
         try (InputStream in = url.openStream();
              GZIPInputStream gzipIn = new GZIPInputStream(in);
-             FileOutputStream out = new FileOutputStream(localFilePath)) {
+             FileOutputStream out = new FileOutputStream(importProperties.getProperty("localFilePath"))) {
 
             byte[] buffer = new byte[1024];
             int bytesRead;
