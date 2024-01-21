3a63bdec0dc657b132671a658be85aedc4825950
Yannick Boyer <yannickboyer25@gmail.com>
2024-01-21 18:34:45 +0100

    Clean Swagger

--- DVF/src/main/java/fr/esgi/dvf/repository/PdfRepository.java
+++ DVF/src/main/java/fr/esgi/dvf/repository/PdfRepository.java
@@ -2,7 +2,9 @@ package fr.esgi.dvf.repository;
 
 import fr.esgi.dvf.business.Pdf;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface PdfRepository extends JpaRepository<Pdf, Long> {
 
 }
