8efbc88162d17b9b4372e3f0eb4711e9f2300db2
Yannick Boyer <yannickboyer25@gmail.com>
2024-01-22 23:58:45 +0100

    Modify API request to get PDF, open and download it + update front page

--- DVF-front/src/components/Map.vue
+++ DVF-front/src/components/Map.vue
@@ -16,9 +16,8 @@ import { ref, onMounted } from 'vue';
 import Form from './Form.vue'
 
 const handleSubmitForm = (formData) => {
-  console.log('formData', formData);
     // On utilise Axios pour envoyer une requête GET à l'API
-    axios.get('http://localhost:8080/LigneTransaction/generatePdfByLocationParam', {
+    axios.get('http://localhost:8080/LigneTransaction/generatePdfByLocation', {
       headers: {
         'Content-Type': 'application/json'
       },
@@ -26,13 +25,27 @@ const handleSubmitForm = (formData) => {
         "longitude": formData.longitude,
         "latitude": formData.latitude,
         "rayon": formData.radius
-      }
+      },
+      responseType: 'arraybuffer' // Définit le type de réponse attendu comme un tableau d'octets
     })
     .then(response => {
-        console.log(response.data);
+        const pdfData = new Blob([response.data], { type: 'application/pdf' });
+        const pdfUrl = URL.createObjectURL(pdfData);
+        window.open(pdfUrl);
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfUrl;
+        downloadLink.download = `document_${formData.latitude}_${formData.longitude}_${formData.radius}.pdf`;
+        downloadLink.click();
     })
     .catch(error => {
         console.error(error);
+        if (error.response != null) {
+          const textContent = new TextDecoder('utf-8').decode(error.response.data);
+          alert(`${textContent}`);
+        }
+        else {
+          alert("L'API n'est pas disponible");
+        }
     });
 };
 
